ce126548686b7d5a4708f5f6abaf5efa281a1e87
Mahdi <mahdiraafat3@gmail.com>
2024-03-06 06:10:25 -0500

    Added Customizable Shortcuts

--- src/js/main.js
+++ src/js/main.js
@@ -7,274 +7,227 @@ let mainWindow;
 let tray = null;
 const imagePath = path.join(__dirname, '..', 'assets', 'img');
 
-
-
+const shortcuts = {
+ 'increase-opacity': 'Control+Shift+Up',
+ 'decrease-opacity': 'Control+Shift+Down',
+ 'set-min-opacity': 'Control+Shift+Z',
+ 'set-max-opacity': 'Control+Shift+X',
+ 'quit-app': 'Control+Shift+Q',
+ 'toggle-window': 'Control+Shift+W',
+ 'close-snipping': 'Control+Shift+S',
+ 'toggle-controls': 'Control+Shift+H',
+ 'increase-window-size': 'Control+Shift+=',
+ 'decrease-window-size': 'Control+Shift+-'
+};
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    title: 'Floating Browser',
-    width: 700,
-    height: 600,
-    autoHideMenuBar: true,
-    backgroundColor: '#16171a',
-    show: false,
-    icon: path.join(imagePath, 'StealthPlane.png'),
-    webPreferences: {      
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-      webviewTag: true
-    },
-  });
+mainWindow = new BrowserWindow({
+ title: 'Floating Browser',
+ width: 700,
+ height: 600,
+ autoHideMenuBar: true,
+ backgroundColor: '#16171a',
+ show: false,
+ icon: path.join(imagePath, 'StealthPlane.png'),
+ webPreferences: {    
+  nodeIntegration: true,
+  contextIsolation: false,
+  enableRemoteModule: true,
+  webviewTag: true
+ },
+});
 
 mainWindow.loadFile(path.join(__dirname, '..', 'html', 'index.html'));
 
-  electronContextMenu({
-    window: mainWindow,
-    showInspectElement: true,
-  });
+electronContextMenu({
+ window: mainWindow,
+ showInspectElement: true,
+});
 
-  mainWindow.on('ready-to-show', () => {
-    app.dock && app.dock.hide();
-    mainWindow.show();
-    app.dock && app.dock.show();
+mainWindow.on('ready-to-show', () => {
+ app.dock && app.dock.hide();
+ mainWindow.show();
+ app.dock && app.dock.show();
 
-    mainWindow.setAlwaysOnTop(true, 'screen-saver');
-    mainWindow.setVisibleOnAllWorkspaces(true);
-    mainWindow.setFullScreenable(false);
-  });
+ mainWindow.setAlwaysOnTop(true, 'screen-saver');
+ mainWindow.setVisibleOnAllWorkspaces(true);
+ mainWindow.setFullScreenable(false);
+});
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+mainWindow.on('closed', function () {
+ mainWindow = null;
+});
 
-  createTrayIcon();
+createTrayIcon();
 }
-
 
 function createTrayIcon() {
-  tray = new Tray(path.join(imagePath, 'StealthPlane.png'));
-  
-  const trayContextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Open',
-      icon: path.join(imagePath, 'StealthPlaneSmall.png'),
-      click: () => mainWindow.show()
-    },
-    {
-      label: 'Exit',
-      icon: path.join(imagePath, 'StealthPlaneSmall.png'),
-      click: () => app.quit()
-    }
-  ]);
-  
-  tray.setToolTip('Floating Browser');
-  tray.setContextMenu(trayContextMenu);
+tray = new Tray(path.join(imagePath, 'StealthPlane.png'));
+ 
+const trayContextMenu = Menu.buildFromTemplate([
+ {
+  label: 'Open',
+  icon: path.join(imagePath, 'StealthPlaneSmall.png'),
+  click: () => mainWindow.show()
+ },
+ {
+  label: 'Exit',
+  icon: path.join(imagePath, 'StealthPlaneSmall.png'),
+  click: () => app.quit()
+ }
+]);
+ 
+tray.setToolTip('Floating Browser');
+tray.setContextMenu(trayContextMenu);
 
-  tray.on('click', () => {
-    mainWindow.show();
-  });
+tray.on('click', () => {
+ mainWindow.show();
+});
 }
-
-// Snip
 
 class SnippingTool {
-    constructor() {
-        this.snipWindow = null;
-        this.fullScreenshot = null;
+ constructor() {
+   this.snipWindow = null;
+   this.fullScreenshot = null;
 
-        this.attachEventListeners();
-    }
+   this.attachEventListeners();
+ }
 
-    publicCloseSnipWindow() {
-        this.closeSnipWindow();
-    }
-	
-    attachEventListeners() {
-        ipcMain.on('start-snipping', this.startSnipping.bind(this));
-        ipcMain.on('capture-portion', this.capturePortion.bind(this));
-        ipcMain.on('capture-screen', this.captureEntireScreen.bind(this));
-    }
+ publicCloseSnipWindow() {
+   this.closeSnipWindow();
+ }
+   
+ attachEventListeners() {
+   ipcMain.on('start-snipping', this.startSnipping.bind(this));
+   ipcMain.on('capture-portion', this.capturePortion.bind(this));
+   ipcMain.on('capture-screen', this.captureEntireScreen.bind(this));
+ }
 
-    async captureScreen() {
-        const primaryDisplayBounds = screen.getPrimaryDisplay().bounds;
-        const sources = await desktopCapturer.getSources({ 
-            types: ['screen'], 
-            thumbnailSize: primaryDisplayBounds 
-        });
+ async captureScreen() {
+   const primaryDisplayBounds = screen.getPrimaryDisplay().bounds;
+   const sources = await desktopCapturer.getSources({ 
+     types: ['screen'], 
+     thumbnailSize: primaryDisplayBounds 
+   });
 
-        if (sources && sources[0]?.thumbnail) {
-            return nativeImage.createFromDataURL(sources[0].thumbnail.toDataURL());
-        }
+   if (sources && sources[0]?.thumbnail) {
+     return nativeImage.createFromDataURL(sources[0].thumbnail.toDataURL());
+   }
 
-        console.error('Failed to capture the screen');
-        dialog.showErrorBox('Error', 'Failed to capture the screen.');
-        return null;
-    }
+   console.error('Failed to capture the screen');
+   dialog.showErrorBox('Error', 'Failed to capture the screen.');
+   return null;
+ }
 
-    async startSnipping() {
-        if (this.snipWindow) {
-            console.log('Snipping window already open.');
-            return;
-        }
-        
-        this.fullScreenshot = await this.captureScreen();
-        this.openSnipWindow();
-    }
-
-    openSnipWindow() {
-        const primaryDisplayBounds = screen.getPrimaryDisplay().bounds;
-
-        this.snipWindow = new BrowserWindow({
-            ...primaryDisplayBounds,
-            frame: false,
-            transparent: true,
-            alwaysOnTop: true,
-			fullscreen: true,   
-           skipTaskbar: true,
-		   resizable: false,
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false
-            }
-			
-        });
-
-      this.snipWindow.once('ready-to-show', () => {
-        this.snipWindow.focus();
-        this.snipWindow.maximize();						
-        this.snipWindow.setAlwaysOnTop(true, 'screen-saver', 1);
-        this.snipWindow.setFullScreen(true);
-    });
-
-    this.snipWindow.loadFile(path.join(__dirname, '..', 'html', 'snip.html'));
-
-
-
-    }
-
-    capturePortion(_, coords) {
-    if (!this.fullScreenshot) return;
+ async startSnipping() {
+   if (this.snipWindow) {
+     console.log('Snipping window already open.');
+     return;
+   }
     
-    // Validate and log coords
-    if (typeof coords.x !== 'number' || typeof coords.y !== 'number' || typeof coords.width !== 'number' || typeof coords.height !== 'number') {
-        console.error('Invalid coordinates:', coords);
-        return;
-    }
+   this.fullScreenshot = await this.captureScreen();
+   this.openSnipWindow();
+ }
 
-    const snippedImage = this.fullScreenshot.crop(coords);
-    require('electron').clipboard.writeImage(snippedImage);
-    
-    this.closeSnipWindow();
-    this.fullScreenshot = null; 
+ openSnipWindow() {
+   const primaryDisplayBounds = screen.getPrimaryDisplay().bounds;
+
+   this.snipWindow = new BrowserWindow({
+     ...primaryDisplayBounds,
+     frame: false,
+     transparent: true,
+     alwaysOnTop: true,
+   		fullscreen: true,  
+     skipTaskbar: true,
+   	  resizable: false,
+     webPreferences: {
+       nodeIntegration: true,
+       contextIsolation: false
+     }
+
+   });
+
+  this.snipWindow.once('ready-to-show', () => {
+   this.snipWindow.focus();
+   this.snipWindow.maximize();						
+   this.snipWindow.setAlwaysOnTop(true, 'screen-saver', 1);
+   this.snipWindow.setFullScreen(true);
+ });
+
+ this.snipWindow.loadFile(path.join(__dirname, '..', 'html', 'snip.html'));
+ }
+
+ capturePortion(_, coords) {
+ if (!this.fullScreenshot) return;
+  
+ if (typeof coords.x !== 'number' || typeof coords.y !== 'number' || typeof coords.width !== 'number' || typeof coords.height !== 'number') {
+   console.error('Invalid coordinates:', coords);
+   return;
+ }
+
+ const snippedImage = this.fullScreenshot.crop(coords);
+ require('electron').clipboard.writeImage(snippedImage);
+  
+ this.closeSnipWindow();
+ this.fullScreenshot = null; 
 }
 
+ closeSnipWindow() {
+   if (this.snipWindow) {
+     this.snipWindow.close();
+     this.snipWindow = null;
+   }
+ }
 
-    closeSnipWindow() {
-        if (this.snipWindow) {
-            this.snipWindow.close();
-            this.snipWindow = null;
-        }
-    }
-
-    async captureEntireScreen() {
-        this.fullScreenshot = await this.captureScreen();
-        if (!this.fullScreenshot) {
-            console.error('Error capturing screen');
-        }
-    }
+ async captureEntireScreen() {
+   this.fullScreenshot = await this.captureScreen();
+   if (!this.fullScreenshot) {
+     console.error('Error capturing screen');
+   }
+ }
 }
 let snippingTool = new SnippingTool();
 
-
-
-
-// Url nav
-
 ipcMain.on('navigate', (event, url) => {
-  event.sender.send('load-webview', url);
+event.sender.send('load-webview', url);
 });
-
-
 
 ipcMain.on('set-opacity', (event, value) => {
-  mainWindow.setOpacity(value);
+mainWindow.setOpacity(value);
 });
 
-
+ipcMain.on('update-shortcut', (event, { action, newShortcut }) => {
+ if (action in shortcuts && /Control\+Shift\+[A-Za-z]/.test(newShortcut)) {
+   shortcuts[action] = newShortcut;
+   globalShortcut.unregister(shortcuts[action]);
+   globalShortcut.register(shortcuts[action], () => {
+     mainWindow.webContents.send(action);
+   });
+ } else {
+   event.sender.send('invalid-shortcut');
+ }
+});
 
 app.on('ready', () => {
-  createWindow();
+createWindow();
 
-  // Registering global shortcuts
-  globalShortcut.register('Control+Shift+Up', () => {
-    mainWindow.webContents.send('increase-opacity');
+Object.entries(shortcuts).forEach(([action, shortcut]) => {
+  globalShortcut.register(shortcut, () => {
+    mainWindow.webContents.send(action);
   });
-  globalShortcut.register('Control+Shift+Down', () => {
-    mainWindow.webContents.send('decrease-opacity');
-  });
-  globalShortcut.register('Control+Shift+Z', () => {
-        mainWindow.webContents.send('set-min-opacity');
-    });
-  globalShortcut.register('Control+Shift+X', () => {
-        mainWindow.webContents.send('set-max-opacity');
-    });
-	globalShortcut.register('Control+Shift+Q', () => {
-        app.quit();
-    });
-	 globalShortcut.register('Control+Shift+W', () => {
-        if (mainWindow.isMinimized()) {
-            mainWindow.restore();
-        } else {
-            mainWindow.focus();
-        }
-    });
-	 globalShortcut.register('Control+Shift+S', () => {
-        snippingTool.publicCloseSnipWindow();
-    });
-	globalShortcut.register('Control+Shift+H', () => {
-  mainWindow.webContents.send('toggle-controls');
 });
-
-globalShortcut.register('Control+Shift+=', () => {
-  const [width, height] = mainWindow.getSize();
-  const [x, y] = mainWindow.getPosition();
-  const newWidth = width + 100;
-  const newHeight = height + 100;
-  const newX = x - 50; 
-  const newY = y - 50; 
- 
-  mainWindow.setSize(newWidth, newHeight);
-  mainWindow.setPosition(newX, newY);
-});
-
-globalShortcut.register('Control+Shift+-', () => {
-  const [width, height] = mainWindow.getSize();
-  const [x, y] = mainWindow.getPosition();
-  if (width > 100 && height > 100) {
-    const newWidth = width - 100;
-    const newHeight = height - 100;
-  
-    const newX = x + 50; 
-    const newY = y + 50; 
-    mainWindow.setSize(newWidth, newHeight);
-    mainWindow.setPosition(newX, newY);
-  }
-});
-
 });
 
 app.on('window-all-closed', function () {
-  app.quit();
+app.quit();
 });
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+if (mainWindow === null) {
+ createWindow();
+}
 });
 
 app.on('will-quit', () => {
-  // Unregister all shortcuts.
-  globalShortcut.unregisterAll();
+globalShortcut.unregisterAll();
 });
--- src/js/preload.js
+++ src/js/preload.js
@@ -1,233 +1,214 @@
 const { ipcRenderer, clipboard, nativeImage } = require('electron');
 
 function isValidURL(str) {
-    const pattern = new RegExp('^(https?:\\/\\/)?' +
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-        '((\\d{1,3}\\.){3}\\d{1,3}))' +
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-        '(\\?[;&a-z\\d%_.~+=-]*)?' +
-        '(\\#[-a-z\\d_]*)?$', 'i');
-    return !!pattern.test(str);
+ const pattern = new RegExp('^(https?:\\/\\/)?' +
+   '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+   '((\\d{1,3}\\.){3}\\d{1,3}))' +
+   '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+   '(\\?[;&a-z\\d%_.~+=-]*)?' +
+   '(\\#[-a-z\\d_]*)?$', 'i');
+ return !!pattern.test(str);
 }
 window.addEventListener('DOMContentLoaded', () => {
 const webview = document.querySelector('webview');
 
-
-// Snipping function
-
-
 class SnippingTool {
-    constructor() {
-        this.startX = null;
-        this.startY = null;
-        this.isDrawing = false;
-        this.rect = document.getElementById('rect');
+ constructor() {
+   this.startX = null;
+   this.startY = null;
+   this.isDrawing = false;
+   this.rect = document.getElementById('rect');
 
-        this.attachEventListeners();
-    }
+   this.attachEventListeners();
+ }
 
-    attachEventListeners() {
-        document.addEventListener('mousedown', this.handleMouseDown.bind(this));
-        document.addEventListener('mousemove', this.handleMouseMove.bind(this));
-        document.addEventListener('mouseup', this.handleMouseUp.bind(this));
+ attachEventListeners() {
+   document.addEventListener('mousedown', this.handleMouseDown.bind(this));
+   document.addEventListener('mousemove', this.handleMouseMove.bind(this));
+   document.addEventListener('mouseup', this.handleMouseUp.bind(this));
 
-        document.getElementById('snip-button').addEventListener('click', this.initiateSnipping.bind(this));
-    }
+   document.getElementById('snip-button').addEventListener('click', this.initiateSnipping.bind(this));
+ }
 
-    handleMouseDown(e) {
-        if (!this.isDrawing ) {
-            this.startX = e.clientX;
-            this.startY = e.clientY;
-            this.isDrawing = true;
-			if (this.rect) {  // Check if rect is not null
-            this.rect.style.visibility = 'visible';
-        }
-        }
-    }
+ handleMouseDown(e) {
+   if (!this.isDrawing ) {
+     this.startX = e.clientX;
+     this.startY = e.clientY;
+     this.isDrawing = true;
+   		if (this.rect) {
+     this.rect.style.visibility = 'visible';
+   }
+   }
+ }
 
-    handleMouseMove(e) {
-    if (this.rect && this.isDrawing && this.startX !== null) {
-        const { width, height, left, top } = this.calculateDimensions(e.clientX, e.clientY);
+ handleMouseMove(e) {
+ if (this.rect && this.isDrawing && this.startX !== null) {
+   const { width, height, left, top } = this.calculateDimensions(e.clientX, e.clientY);
 
-        this.rect.style.width = `${width}px`;
-        this.rect.style.height = `${height}px`;
-        this.rect.style.left = `${left}px`;
-        this.rect.style.top = `${top}px`;
-    }
+   this.rect.style.width = `${width}px`;
+   this.rect.style.height = `${height}px`;
+   this.rect.style.left = `${left}px`;
+   this.rect.style.top = `${top}px`;
+ }
 }
 
+ handleMouseUp(e) {
+ if (this.isDrawing) {
+   const { width, height, left, top } = this.calculateDimensions(e.clientX, e.clientY);
 
-    handleMouseUp(e) {
-    if (this.isDrawing) {
-        const { width, height, left, top } = this.calculateDimensions(e.clientX, e.clientY);
-
-        ipcRenderer.send('capture-portion', {
-            x: left, 
-            y: top,  
-            width,
-            height
-        });
-        this.reset();
-    }
+   ipcRenderer.send('capture-portion', {
+     x: left, 
+     y: top,  
+     width,
+     height
+   });
+   this.reset();
+ }
 }
 
+ calculateDimensions(currentX, currentY) {
+ const width = Math.abs(currentX - this.startX);
+ const height = Math.abs(currentY - this.startY);
+ const left = Math.min(currentX, this.startX);
+ const top = Math.min(currentY, this.startY);
 
-    calculateDimensions(currentX, currentY) {
-    const width = Math.abs(currentX - this.startX);
-    const height = Math.abs(currentY - this.startY);
-    const left = Math.min(currentX, this.startX);
-    const top = Math.min(currentY, this.startY);
-
-    return { width, height, left, top };
+ return { width, height, left, top };
 }
 
+ initiateSnipping() {
+   ipcRenderer.send('start-snipping');
+ }
 
-    initiateSnipping() {
-        ipcRenderer.send('start-snipping');
-    }
+ reset() {
+   this.startX = null;
+   this.startY = null;
+   this.isDrawing = false;
+   	 if (this.rect) {  
+   this.rect.style.visibility = 'hidden';
+ }
 
-    reset() {
-        this.startX = null;
-        this.startY = null;
-        this.isDrawing = false;
-		 if (this.rect) {  
-        this.rect.style.visibility = 'hidden';
-    }
-
-    }
+ }
 }
 
+ new SnippingTool();
 
-    new SnippingTool();
+document.getElementById('url-bar').addEventListener('keydown', (e) => {
+   if (e.key === 'Enter') {
+     let url = e.target.value;
+     if (!isValidURL(url)) {
+       url = `https://www.google.com/search?q=${encodeURIComponent(url)}`;
+     } else if (!url.startsWith('http://') && !url.startsWith('https://')) {
+       url = 'https://' + url;
+     }
+     ipcRenderer.send('navigate', url);
+   }
+ });
 
-
-// End
-
-
-
-//URL BAR
-    document.getElementById('url-bar').addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') {
-            let url = e.target.value;
-            if (!isValidURL(url)) {
-                url = `https://www.google.com/search?q=${encodeURIComponent(url)}`;
-            } else if (!url.startsWith('http://') && !url.startsWith('https://')) {
-                url = 'https://' + url;
-            }
-            ipcRenderer.send('navigate', url);
-        }
-    });
-
-    document.getElementById('brightness-slider').addEventListener('input', (e) => {
-        const opacity = e.target.value;
-        ipcRenderer.send('set-opacity', parseFloat(opacity));
-    });
-	
+ document.getElementById('brightness-slider').addEventListener('input', (e) => {
+   const opacity = e.target.value;
+   ipcRenderer.send('set-opacity', parseFloat(opacity));
+ });
+   
 const backButton = document.getElementById('back-button');
 const forwardButton = document.getElementById('forward-button');
 const webView = document.querySelector('webview');
 
-// Check if navigation is possible
 function updateNavButtons() {
-    if (webView.canGoBack()) {
-        backButton.classList.remove('disabled-button');
-    } else {
-        backButton.classList.add('disabled-button');
-    }
-  
-    if (webView.canGoForward()) {
-        forwardButton.classList.remove('disabled-button');
-    } else {
-        forwardButton.classList.add('disabled-button');
-    }
+ if (webView.canGoBack()) {
+   backButton.classList.remove('disabled-button');
+ } else {
+   backButton.classList.add('disabled-button');
+ }
+ 
+ if (webView.canGoForward()) {
+   forwardButton.classList.remove('disabled-button');
+ } else {
+   forwardButton.classList.add('disabled-button');
+ }
 }
 
-// Add event listener for when the webview's navigation state changes
 webView.addEventListener('did-navigate', updateNavButtons);
 webView.addEventListener('did-navigate-in-page', updateNavButtons);
 
-    document.getElementById('back-button').addEventListener('click', () => {
-        if (webview.canGoBack()) {
-            webview.goBack();
-        }
-    });
+ document.getElementById('back-button').addEventListener('click', () => {
+   if (webview.canGoBack()) {
+     webview.goBack();
+   }
+ });
 document.getElementById('forward-button').addEventListener('click', () => {
-    if (webview.canGoForward()) {
-        webview.goForward();
-    }
+ if (webview.canGoForward()) {
+   webview.goForward();
+ }
 });
 
 function hidePrefixAndRemoveBackslashes(url) {
-    let modifiedUrl = url.replace(/^https:\/\/(www\.)?/, '');
-    modifiedUrl = modifiedUrl.replace(/\/+$/, ''); 
-    return modifiedUrl;
+ let modifiedUrl = url.replace(/^https:\/\/(www\.)?/, '');
+ modifiedUrl = modifiedUrl.replace(/\/+$/, ''); 
+ return modifiedUrl;
 }
-   
+ 
 function toggleControls() {
-  const controls = document.getElementById('controls');
-  if (controls.style.display === 'none') {
-    controls.style.display = 'flex';
-  } else {
-    controls.style.display = 'none';
-  }
+const controls = document.getElementById('controls');
+if (controls.style.display === 'none') {
+ controls.style.display = 'flex';
+} else {
+ controls.style.display = 'none';
+}
 }
 
 ipcRenderer.on('toggle-controls', toggleControls);
 
-
 webview.addEventListener('did-navigate', (event) => {
-    const urlBar = document.getElementById('url-bar');
-    urlBar.value = event.url;
-    urlBar.dataset.fullUrl = event.url; 
-    urlBar.value = hidePrefixAndRemoveBackslashes(event.url); 
+ const urlBar = document.getElementById('url-bar');
+ urlBar.value = event.url;
+ urlBar.dataset.fullUrl = event.url; 
+ urlBar.value = hidePrefixAndRemoveBackslashes(event.url); 
 });
-
 
 document.getElementById('url-bar').addEventListener('focus', (event) => {
-    const urlBar = event.target;
-    if (urlBar.dataset.fullUrl) {
-        urlBar.value = urlBar.dataset.fullUrl; 
-    }
+ const urlBar = event.target;
+ if (urlBar.dataset.fullUrl) {
+   urlBar.value = urlBar.dataset.fullUrl; 
+ }
 });
-
 
 document.getElementById('url-bar').addEventListener('blur', (event) => {
-    const urlBar = event.target;
-    if (urlBar.dataset.fullUrl) {
-        urlBar.value = hidePrefixAndRemoveBackslashes(urlBar.dataset.fullUrl);
-    }
+ const urlBar = event.target;
+ if (urlBar.dataset.fullUrl) {
+   urlBar.value = hidePrefixAndRemoveBackslashes(urlBar.dataset.fullUrl);
+ }
 });
 
+ ipcRenderer.on('load-webview', (event, url) => {
+   webview.setAttribute('src', url);
+});
 
-    ipcRenderer.on('load-webview', (event, url) => {
-        webview.setAttribute('src', url);
-    });
+ipcRenderer.on('increase-opacity', () => {
+  const brightnessSlider = document.getElementById('brightness-slider');
+  brightnessSlider.value = Math.min(brightnessSlider.max, parseFloat(brightnessSlider.value) + 0.01);
+  ipcRenderer.send('set-opacity', parseFloat(brightnessSlider.value));
+});
 
-    ipcRenderer.on('increase-opacity', () => {
-        const brightnessSlider = document.getElementById('brightness-slider');
-        brightnessSlider.value = Math.min(brightnessSlider.max, parseFloat(brightnessSlider.value) + 0.01);
-        ipcRenderer.send('set-opacity', parseFloat(brightnessSlider.value));
-    });
+ipcRenderer.on('decrease-opacity', () => {
+  const brightnessSlider = document.getElementById('brightness-slider');
+  brightnessSlider.value = Math.max(brightnessSlider.min, parseFloat(brightnessSlider.value) - 0.01);
+  ipcRenderer.send('set-opacity', parseFloat(brightnessSlider.value));
+});
 
-    ipcRenderer.on('decrease-opacity', () => {
-        const brightnessSlider = document.getElementById('brightness-slider');
-        brightnessSlider.value = Math.max(brightnessSlider.min, parseFloat(brightnessSlider.value) - 0.01);
-        ipcRenderer.send('set-opacity', parseFloat(brightnessSlider.value));
-    });
-
-    ipcRenderer.on('set-min-opacity', () => {
-        const brightnessSlider = document.getElementById('brightness-slider');
-        brightnessSlider.value = brightnessSlider.min;
-        ipcRenderer.send('set-opacity', parseFloat(brightnessSlider.value));
-    });
-    ipcRenderer.on('set-max-opacity', () => {
-        const brightnessSlider = document.getElementById('brightness-slider');
-        brightnessSlider.value = brightnessSlider.max;
-        ipcRenderer.send('set-opacity', parseFloat(brightnessSlider.value));
-    });
-	document.getElementById('settings-button').addEventListener('click', function() {
+ipcRenderer.on('set-min-opacity', () => {
+  const brightnessSlider = document.getElementById('brightness-slider');
+  brightnessSlider.value = brightnessSlider.min;
+  ipcRenderer.send('set-opacity', parseFloat(brightnessSlider.value));
+});
+ipcRenderer.on('set-max-opacity', () => {
+  const brightnessSlider = document.getElementById('brightness-slider');
+  brightnessSlider.value = brightnessSlider.max;
+  ipcRenderer.send('set-opacity', parseFloat(brightnessSlider.value));
+});
+  document.getElementById('settings-button').addEventListener('click', function() {
 window.location.href = "../html/Settings.html";
 });
+
 
 
 });
